b6213c1a9c6c6eddf9bb5b5290f2ebfc7afe7af5
kametronon <e@mail>
2025-06-27 01:00:50 +0300

    sched

--- backend/src/main/kotlin/com/priority/tasktracker/TaskTrackerApplication.kt
+++ backend/src/main/kotlin/com/priority/tasktracker/TaskTrackerApplication.kt
@@ -2,7 +2,9 @@ package com.priority.tasktracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class TaskTrackerApplication
 
